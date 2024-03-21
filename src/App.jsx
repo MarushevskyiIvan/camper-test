d4ea6dad3466cda066ece950b6fe7dffd4dc3486
Marushevskyi Ivan <131617971+MarushevskyiIvan@users.noreply.github.com>
2024-03-22 01:30:59 +0200

    render fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, NavLink } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import CatalogPage from './pages/CatalogPage'
 import FavoritesPage from './pages/FavoritesPage'
@@ -10,14 +10,28 @@ import { Reviews } from './components/reviews/Reviews'
 function App() {
 	return (
 		<>
+			<header>
+				<ul>
+					<li>
+						<NavLink to='/'>Home</NavLink>
+					</li>
+					<li>
+						<NavLink to='/favorite'>Favorite</NavLink>
+					</li>
+					<li>
+						<NavLink to='/catalog'>Catalog</NavLink>
+					</li>
+				</ul>
+			</header>
 			<Routes>
-				<Route path='/' element={<AppLayout />} />
-				<Route index element={<HomePage />} />
-				<Route path='/catalog' element={<CatalogPage />} />
+				<Route path='/' element={<AppLayout />}>
+					<Route index element={<HomePage />} />
+					<Route path='/catalog' element={<CatalogPage />} />
 
-				<Route path='/favorites' element={<FavoritesPage />}>
-					<Route path='/features' element={<Features />} />
-					<Route path='/reviews' element={<Reviews />} />
+					<Route path='/favorites' element={<FavoritesPage />}>
+						<Route path='features' element={<Features />} />
+						<Route path='reviews' element={<Reviews />} />
+					</Route>
 				</Route>
 			</Routes>
 		</>
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<BrowserRouter basename='camper-test'>
+		<BrowserRouter
+		// basename='/camper-test'
+		>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
